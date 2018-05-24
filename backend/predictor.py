1b7b3f0834177676107d786c0d3bf07f21c3348f
Tal Machani <tal.machani@idomoo.com>
2018-05-24 22:29:58 +0300

    new server by date epoch and step

--- backend/predictor.py
+++ backend/predictor.py
@@ -22,7 +22,7 @@ def get_model(checkpoint_path):
 def predict(expt_dir, seq_str, date, epoch, step, n=3):
     seq = seq_str.strip().split()
     checkpoint_path = os.path.join(Checkpoint.CHECKPOINT_DIR_NAME, date, epoch, step)
-    seq2seq, input_vocab, output_vocab = get_model(expt_dir, checkpoint_path)
+    seq2seq, input_vocab, output_vocab = get_model(checkpoint_path)
     beam_search = Seq2seq(seq2seq.encoder, TopKDecoder(seq2seq.decoder, 4))
     predictor = Predictor(beam_search, input_vocab, output_vocab)
     return predictor.predict_n(seq, n=n)
